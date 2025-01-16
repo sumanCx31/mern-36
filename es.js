91461d4421884dba424b4e7aa2bd3ae5b2186292
sumancx31 <sumansah029@gmail.com>
2025-01-16 10:36:35 +0545

    es file created

---
+++ es.js
@@ -0,0 +1,17 @@
+let logError=
+{
+    name:"suman",
+    age:43,
+    address:"saptary"
+}
+ const student={
+    name:"Aayush",
+    rn:2
+}
+const person = {
+    name:"Peter",
+    address:"New york"
+}
+logError.address="kathmandu"
+module.exports = {logError,student,person}
+
